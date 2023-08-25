86c09474f57c5fef80541661d981206a8112cb3a
o_odong <skai0907@gmail.com>
2023-08-25 09:13:55 +0900

    0825 P38 Simulation C++로 만들기

---
+++ Source/Project0824/MyPawn.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MyPawn.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UArrowComponent;
+class UFloatingPawnMovement;
+class UMyActorComponent;
+
+UCLASS()
+class PROJECT0824_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 어딘가에 있는 BoxComponent를 사용하는 것이므로 메모리에 접근하여 사용.
+	// 그래서 모두 포인터로 사용.
+	// UBoxComponent* 
+
+};
---
+++ Source/Project0824/Project0824.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project0824.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project0824, "Project0824" );
